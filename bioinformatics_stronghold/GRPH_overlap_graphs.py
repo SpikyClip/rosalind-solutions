abd9aaa2fa58b89a91b687b4aa3a359f4f5c22ef
SpikyClip <74215773+SpikyClip@users.noreply.github.com>
2021-02-26 18:06:32 +1100

    Added exercise information to GRPH

--- bioinformatics_stronghold/GRPH_overlap_graphs.py
+++ bioinformatics_stronghold/GRPH_overlap_graphs.py
@@ -1,3 +1,31 @@
+# url: http://rosalind.info/problems/grph/
+
+# Problem
+
+# A graph whose nodes have all been labeled can be represented by an
+# adjacency list, in which each row of the list contains the two node
+# labels corresponding to a unique edge.
+
+# A directed graph (or digraph) is a graph containing directed edges, each
+# of which has an orientation. That is, a directed edge is represented by
+# an arrow instead of a line segment; the starting and ending nodes of an
+# edge form its tail and head, respectively. The directed edge with tail v
+# and head w is represented by (v,w) (but not by (w,v)). A directed loop
+# is a directed edge of the form (v,v).
+
+# For a collection of strings and a positive integer k, the overlap graph
+# for the strings is a directed graph Ok in which each string is
+# represented by a node, and string s is connected to string t with a
+# directed edge when there is a length k suffix of s that matches a length
+# k prefix of t, as long as s≠t; we demand s≠t to prevent directed loops
+# in the overlap graph (although directed cycles may be present).
+
+# Given: A collection of DNA strings in FASTA format having total length
+# at most 10 kbp.
+
+# Return: The adjacency list corresponding to O3. You may return edges in
+# any order.
+
 from common import fasta_to_dict
 
 
@@ -10,8 +38,8 @@ def adj_list(seq_dict, k):
         seq_dict[name] = (seq[:k], seq[-k:])
     # Cycles through each string's suffix, comparing it to all other
     # prefix's (except its own)
-    for name_1, (prefix_1, suffix_1) in seq_dict.items():
-        for name_2, (prefix_2, suffix_2) in seq_dict.items():
+    for name_1, (_, suffix_1) in seq_dict.items():
+        for name_2, (prefix_2, _) in seq_dict.items():
             if name_1 is name_2:
                 continue
             else:
